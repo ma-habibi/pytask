74e93414e57c87ababc1400e91438ed9bbf02714
metflex <106245122+metflekx@users.noreply.github.com>
2024-08-28 09:07:09 +0300

    filters done, most dat a processing on s and c done

--- sc.py
+++ sc.py
@@ -15,6 +15,7 @@ Desc. :
 # Built-ins
 import sys # For argv
 from io import BytesIO
+from datetime import date # for today
 
 # Third-party
 import requests
@@ -91,7 +92,7 @@ def server(csvpath):
                      on="kurzname",
                      how="outer",
                      validate="one_to_many",
-                     suffixes=('', '_res'))
+                     suffixes=("", "_res"))
 
     # Combine labelId and LabelId_res
     resdf['labelIds'] = resdf[
@@ -105,7 +106,7 @@ def server(csvpath):
     # labelIds resolve its colorCode
     for i, row in resdf.iterrows():
         # Skip NaN
-        if pd.notna(row['labelIds']):
+        if pd.notna(row["labelIds"]):
             res = requests.get(
                 "https://api.baubuddy.de/"
                 "dev/index.php/v1/labels/"
@@ -119,11 +120,16 @@ def server(csvpath):
                 continue
 
             # set colorCode
-            resdf.at[i, 'labelIds']=\
-            res.json()[0]['colorCode']
+            resdf.at[i, "labelIds"]=\
+            res.json()[0]["colorCode"]
 
     # return data-structure in JSON format
     return resdf.to_json()
+
+
+GREEN = "007500"
+ORANGE = "FFA500"
+RED = "b30000"
 
 
 def df_towb(df):
@@ -138,6 +144,31 @@ def df_towb(df):
     xlbuf.seek(0) # seek begin
     wb = load_workbook(xlbuf) # load wb.
     return wb
+
+def get_colors(df):
+    """ Make a new column colortmp
+    in df Obtain the row color 
+    according to the hu value.
+    """
+
+    df["colortmp"] = None 
+    today = int(str(
+        date.today()).replace(
+            "-", ""))
+    twelve_m = today - 10000
+    three_m = today - 300
+    for i, huval in enumerate(df["hu"]):
+        if isinstance(huval, str):
+            huval = int(
+                    huval.replace(
+                        "-", "")) 
+            if huval < twelve_m:
+                if huval < three_m:
+                    df.at[i, "colortmp"] = GREEN
+                    continue
+                df.at[i, "colortmp"] = ORANGE
+                continue 
+            df.at[i, "colortmp"] = RED
 
 def tint_labelids(ws):
     """ If labelIds are given 
@@ -155,8 +186,9 @@ def tint_labelids(ws):
 
     i = 2 # skip the keys
     j = 0
-    while i < ws.max_row and j < ws.max_column:
-        if ws[1][j].value != 'labelIds':
+    while i < ws.max_row and\
+            j < ws.max_column + 1:
+        if ws[1][j].value != "labelIds":
             j += 1
             continue
 
@@ -173,7 +205,42 @@ def tint_labelids(ws):
         
         i += 1
 
-    return ws
+def paint_rows(ws) -> int:
+    """ Use the values stored
+    in colortmp, to paint each
+    row according to hu value.
+    returns the index at which
+    the colortmp exist.
+    in a call to paint_rows the
+    return value must be used
+    to delete tmp column.
+    """
+
+    # find the colortmp index
+    cti = 0
+    while cti < ws.max_column and\
+    ws[1][cti].value != "colortmp":
+        cti += 1
+
+    i = 2
+    while i < ws.max_row + 1:
+        # Get color
+        color = ws[i][cti].value
+        if isinstance(color, str):
+            # Paint row
+            j = 0
+            fill = PatternFill(
+                    start_color=color,
+                    end_color=color,
+                    fill_type="solid")
+            while j < ws.max_column:
+                ws[i][j].fill = fill
+                j += 1
+
+        i += 1
+
+    # return index of tmp col
+    return cti + 1
 
 def client():
     """Transmits a CSV to
@@ -185,7 +252,8 @@ def client():
     into account.
 
     TODO:
-        [ ] color cells
+        [*] color cells
+        [ ] handle args
         [ ] seperate files
         [ ] after server listening
             make calls
@@ -196,13 +264,9 @@ def client():
     df = pd.read_json(
             server("vehicles.csv"))
 
-    # Rows are sorted by response
-    # field gruppe
-    df = df.sort_values(by=["gruppe"])
-
+    # handle -k flag
     # Columns always contain rnr field
-    cols = ['rnr']
-
+    cols = ["rnr"]
     # Only keys that match the input
     # arguments are considered as
     # additional columns
@@ -216,6 +280,14 @@ def client():
             continue
         cols.append(arg)
 
+    # -c flag
+    get_colors(df)
+    cols.append("colortmp")
+
+    # Rows are sorted by response
+    # field gruppe
+    df = df.sort_values(by=["gruppe"])
+
     df = df[[col for col in cols]]
     del cols # TODO: later after restapi implementation handle the scope
 
@@ -227,36 +299,18 @@ def client():
 
     # Tint cell with the resolved
     # label id value
-    ws = tint_labelids(ws)
+    tint_labelids(ws)
 
-    wb.save("out.xlsx")
-    print("FINISHED")
+    # Paint each row for colored
+    # output
+    ws.delete_cols(
+            paint_rows(ws), amount=1)
     sys.exit(0)
 
-    sys.exit(0)
-
-    # Save the workbook
-    wb.save("out.xlsx")
-    print("FINISHED")
-    sys.exit(0)
-
-    # Take an input parameter -k/--keys that can receive an arbitrary amount of string arguments
-
-    # Take an input parameter -c/--colored that receives a boolean flag and defaults to True
-
-    # Transmit CSV containing vehicle information to the POST Call of the server (example data: vehicles.csv)
-
-    # Convert the servers response into an excel file that contains all resources and make sure that:
-
-    # TODO:
-        # [*] Rows are sorted by response field gruppe [*]
-        # [*] Columns always contain rnr field [*]
-        # [*] Only keys that match the input arguments are considered as additional columns (i.e. when the script is invoked with kurzname and info, print two extra columns)[*]
-        # [*] If labelIds are given and at least one colorCode could be resolved, use the first colorCode to tint the cell's text (if labelIds is given in -k)
-        # [ ] If the -c flag is True, color each row depending on the following logic:
-        # [ ]   # If hu is not older than 3 months --> green (#007500)
-        # [ ]   # If hu is not older than 12 months --> orange (#FFA500)
-        # [ ]   # If hu is older than 12 months --> red (#b30000)
-        # The file should be named vehicles_{current_date_iso_formatted}.xlsx
+    # [ ] Take an input parameter -k/--keys that can receive an arbitrary amount of string arguments
+    # [ ] Take an input parameter -c/--colored that receives a boolean flag and defaults to True
+    # [ ] Transmit CSV containing vehicle information to the POST Call of the server (example data: vehicles.csv)
+    # [ ] Convert the servers response into an excel file that contains all resources and make sure that:
+    # [ ] The file should be named vehicles_{current_date_iso_formatted}.xlsx
 
 client()
